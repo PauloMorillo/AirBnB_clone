5b9e336c83dfa7d79b192aedf464eeb053e1a0a2
PauloMorillo <pauloan@hotmail.com>
2019-11-09 22:49:39 +0000

    created directories and script for exercises 3 and 4

---
+++ models/base_model.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""This module has the main class called basemodel"""
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """BaseModell class"""
+    def __init__(self, *args, **kwargs):
+        """Here begins the class"""
+        if kwargs:
+            self.id = kwargs["id"]
+            self.created_at = datetime.fromisoformat(kwargs["created_at"])
+            self.updated_at = datetime.fromisoformat(kwargs["updated_at"])
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Method to print class, id, and json"""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
+    def save(self):
+        """Function to save the new update"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary with all keys and values of the instance"""
+        jsdic = dict()
+        for key in self.__dict__:
+            if key == "created_at" or key == "updated_at":
+                value = self.__dict__[key].isoformat()
+            else:
+                value = self.__dict__[key]
+            jsdic[key] = value
+        jsdic["__class__"] = self.__class__.__name__
+        return jsdic
