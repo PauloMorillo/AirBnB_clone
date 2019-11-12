134019fca09ed7b05fa01982b2f9fab77a0ddd84
Daniel E Rodriguez <dr2d4@live.com>
2019-11-12 11:52:52 -0500

    Get objects from class attribute

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,42 +8,39 @@ from models.place import Place
 from models.city import City
 from models.user import User
 import json
+import os
 
 
 class FileStorage:
     """Class to serializes instances to a JSON and desearialize"""
-
-    def __init__(self):
-        """all begins here"""
-        self.__file_path = "file.json"
-        self.__objects = dict()
+    __file_path = "file.json"
+    __objects = dict()
 
     def all(self):
         """return the dictionary onbjects"""
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """assign objects private attribute"""
-        self.__objects[obj.__class__.__name__ + "." + obj.id] = obj
+        FileStorage.__objects[obj.__class__.__name__ + "." + obj.id] = obj
 
     def save(self):
         """Serializes objects private attribute to a JSON file"""
         tmp = {}
-        for obj in self.__objects:
-            tmp.update({obj: self.__objects[obj].to_dict()})
+        for obj in FileStorage.__objects:
+            tmp.update({obj: FileStorage.__objects[obj].to_dict()})
 
         with open(self.__file_path, "w") as f:
             f.write(json.dumps(tmp))
 
     def reload(self):
         """Deserializes the JSON file to objects private attribute"""
-        try:
-            with open(self.__file_path, "r") as f:
-                self.__objects = json.load(f)
 
-            for key, value in self.__objects.items():
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path) as f:
+                FileStorage.__objects = json.load(f)
+
+            for key, value in FileStorage.__objects.items():
                 base = eval(value["__class__"])
 
-                self.__objects[key] = base(**value)
-        except Exception as e:
-            pass
+                FileStorage.__objects[key] = base(**value)
