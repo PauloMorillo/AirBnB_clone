fbdad617885415ab6608c5c423a639b5a555caa5
PauloMorillo <pauloan@hotmail.com>
2019-11-12 20:43:57 +0000

    fixed with strptime

--- models/base_model.py
+++ models/base_model.py
@@ -13,15 +13,12 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Here begins the class"""
         if kwargs:
-            """for key, value in kwargs.items():
+            for key, value in kwargs.items():
                 if key != '__class__':
                     if key in ["created_at", "updated_at"]:
-                        value = datetime.fromisoformat(value)
-
-                    setattr(self, key, value)"""
-            self.id = kwargs["id"]
-            self.created_at = datetime.fromisoformat(kwargs["created_at"])
-            self.updated_at = datetime.fromisoformat(kwargs["updated_at"])
+                        value = datetime.strptime(value,
+                                                  "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
